f8260180620f7f6cef637ff774bd3e9e4fd30b3e
Skavva26 <115889973+skavva-ship-it@users.noreply.github.com>
2024-11-04 22:26:44 -0500

    Initial commit with Next.js React project

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,38 @@
-"use client"
+"use client";
 import TdModule from "../components/TdModule";
-import Hero from "../components/Hero"
+import Hero from "../components/Hero";
 import { CaroCaro } from "@/components/contentCaro";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all"; // Import ScrollTrigger plugin
 
+// Register ScrollTrigger plugin
 
 export default function Home() {
+  gsap.registerPlugin(ScrollTrigger);
   useGSAP(() => {
-    gsap.to(".prot", {
-      y:100,
-      opacity:1,
-    })
+    gsap.to(".prot", { 
+      opacity: 1,      
+      duration: 1,     
+      scrollTrigger: {
+        trigger: ".prot",  
+        start: "top 80%",  
+        end: "bottom top",  
+        scrub: 1,          
+        markers: false,    
+      },
+    });
   });
 
   return (
     <div
       className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
     >
-      <br></br>
+      <br />
       <div className="opacity-0 prot">
-      <Hero />
+        <Hero />
       </div>
-      
+
       <CaroCaro />
       <TdModule />
     </div>
